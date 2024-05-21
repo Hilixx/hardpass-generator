35479495c7773484a2f760d3c33360ed3efb0cb8
unknown <jhonalizan@gmail.com>
2024-05-21 16:20:00 -0300

    solve

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 app.use(express.json());
 const allRoutes = require('./routes/allRoutes');
 const exphbs = require('express-handlebars');
-const port = 10000;
+require('dotenv').config;
+const port = process.env.PORT || 5000;
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.use(express.json());
@@ -13,5 +14,5 @@ app.use(express.urlencoded({
 }));
 app.use('/pass', allRoutes);
 app.listen(port, () => {
-    console.log('servidor rodando na porta 3000');
+    console.log('servidor rodando na porta ' + port);
 });
